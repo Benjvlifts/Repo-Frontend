3829e7f51cadfd5de282f14b1b214493b82ec57c
bENJAMIN <benjissj.vald15@gmail.com>
2026-06-30 06:11:13 -0400

    fix github

--- innovatech-frontend-valdes-munoz/vitest.config.js
+++ innovatech-frontend-valdes-munoz/vitest.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    include: ['src/__tests__/**/*.test.{js,jsx}'],
     setupFiles: ['./src/__tests__/setup.js'],
     coverage: {
       provider: 'v8',
